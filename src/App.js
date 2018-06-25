291cb0930d8f0523c5c3d8758b9300d2dd0951b2
Patrick East <patrick.east@icloud.com>
2018-06-25 12:07:55 -0600

    Added alerts! And now score resets on win or lose.

--- src/App.js
+++ src/App.js
@@ -1,51 +1,56 @@
-import React, { Component } from 'react';
-import './App.css';
-import logo from './logo.svg';
+import React, { Component } from "react";
+import "./App.css";
 import Cards from "./components/Cards";
 import Nav from "./components/Nav";
-import characters from "./characters.json"
+import characters from "./characters.json";
+import logo from './logo.svg';
+import { withAlert } from "react-alert";
 
 class App extends Component {
-
   state = {
     message: "Click an image to begin!",
     topScore: 0,
     currentScore: 0,
     characters: characters,
-    unselectedCharacter: characters,
-  }
+    unselectedCharacter: characters
+  };
 
-  componentDidMount() {
-  }
+  componentDidMount() { }
 
   shuffleArray = array => {
     for (let i = array.length - 1; i > 0; i--) {
       let j = Math.floor(Math.random() * (i + 1));
       [array[i], array[j]] = [array[j], array[i]];
     }
-  }
+  };
 
   lose = () => {
     this.setState({
       message: "You guessed incorrectly!",
-      topScore: (this.state.currentScore > this.state.topScore) ? this.state.currentScore : this.state.topScore,
+      topScore:
+        this.state.currentScore > this.state.topScore
+          ? this.state.currentScore
+          : this.state.topScore,
       currentScore: 0,
       characters: characters,
       unselectedCharacter: characters
     });
-  }
+  };
 
   selectCharacter = name => {
-    
-    const findCharacter = this.state.unselectedCharacter.find(item => item.name === name);
+    const findCharacter = this.state.unselectedCharacter.find(
+      item => item.name === name
+    );
 
     if (findCharacter === undefined) {
       // failure to select a new character
+      this.props.alert.error("Already picked that one, football head!");
       this.lose();
-    }
-    else {
+    } else {
       // success to select a new character
-      const newCharacter = this.state.unselectedCharacter.filter(item => item.name !== name);
+      const newCharacter = this.state.unselectedCharacter.filter(
+        item => item.name !== name
+      );
 
       this.setState({
         message: "You guessed correctly!",
@@ -57,11 +62,22 @@ class App extends Component {
 
     this.shuffleArray(characters);
 
-    if (this.state.currentScore > 10) {
-      var win = document.querySelectorAll(".container");
-      while(win.firstChild) win.removeChild(win.firstChild);
-      alert("You Win!");
-    };
+    if (this.state.currentScore === 11) {
+      const newCharacter = this.state.unselectedCharacter.filter(
+        item => item.name !== name
+      );
+      this.props.alert.success("Maaaaaan! You win!");
+      this.setState({
+        message: "You guessed correctly!",
+        topScore:
+          this.state.currentScore > this.state.topScore
+            ? this.state.currentScore
+            : this.state.topScore,
+        currentScore: 0,
+        characters: characters,
+        unselectedCharacter: newCharacter
+      });
+    }
   };
 
   render() {
@@ -74,30 +90,30 @@ class App extends Component {
         />
         <header>
           <h1>Hey Arnold! Memory Game</h1>
-          <h2>Click on image to earn points, but don't click on any more than once!</h2>
+          <h2>
+            Click on image to earn points, but don't click on any more than
+            once!
+          </h2>
         </header>
         <main className="container">
-          {
-            this.state.characters.map(character => (
-              <Cards
-                key={Math.random()*12}
-                name={character.name}
-                image={character.image}
-                selectCharacter={this.selectCharacter}
-                currentScore={this.state.currentScore}
-              />
-            ))
-          }
+          {this.state.characters.map(character => (
+            <Cards
+              key={Math.random() * 12}
+              name={character.name}
+              image={character.image}
+              selectCharacter={this.selectCharacter}
+              currentScore={this.state.currentScore}
+            />
+          ))}
         </main>
         <footer className="footer">
           <div className="bottom">
             Hey Arnold! Memory Game
-             <img name="Built with ReactJS" alt="Built with ReactJS" src={logo} />
+            <img name="Built with ReactJS" alt="Built with ReactJS" src={logo} />
           </div>
         </footer>
       </div>
     );
   }
 }
-
-export default App;
+export default withAlert(App);
--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import registerServiceWorker from "./registerServiceWorker";
+import { Provider } from "react-alert";
+import AlertTemplate from "react-alert-template-basic";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const options = {
+  timeout: 2000,
+  position: "bottom center"
+};
+
+const Home = () => (
+  <Provider template={AlertTemplate} {...options}>
+    <App />
+  </Provider>
+);
+
+ReactDOM.render(<Home />, document.getElementById("root"));
 registerServiceWorker();
